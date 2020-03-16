25f8c848a0efc27fd9f4f40be0ca9381e0d0fe1b
Andreas Carlsen <alc@tsat.net>
2020-03-16 14:46:28 +0100

    add rootfs overlay to tsat board family

---
+++ board/tsat/3500/rootfs_overlay/usr/local/bin/load_fpga.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+# Usage: load_fpga.sh <path-to-xz-compressed-fpga-image
+
+set -e # fail fast
+
+FW_PATH='/lib/firmware'
+FPGA_FILE="$1"
+STATUS_MSG="Loading FPGA ($FPGA_FILE)"
+
+if [ "$#" -lt "1" ]; then
+  echo "$STATUS_MSG: FAIL (FPGA not specified, e.g. fpga_turbo_high.bit.bin.xz)"
+  exit 1
+fi
+
+if [ ! -f "$FPGA_FILE" ]; then
+  echo "$STATUS_MSG: FAIL (FPGA does not exist)"
+  exit 1
+fi
+
+FPGA_MD5="$(md5sum "$FPGA_FILE" | cut -d' ' -f1)"
+FPGA_NAME="${FPGA_MD5}"
+
+# cleanup the firmware directory
+rm -- $FW_PATH/* 2>/dev/null || echo "/lib/firmware is empty"
+
+# extract the specified FPGA image into the firmware directory
+unxz -c -- "$FPGA_FILE" > "$FW_PATH/$FPGA_NAME"
+
+# take down network interfaces since these will be affected
+ip link set down dev eth0
+ip link set down dev eth1
+
+# load FPGA
+echo 0 > /sys/class/fpga_manager/fpga0/flags
+echo "$FPGA_NAME" > /sys/class/fpga_manager/fpga0/firmware
+
+# bring up network interfaces...twice(bug?)
+sleep 3
+ip link set up dev eth0 || ip link set up dev eth0
+ip link set up dev eth1 || ip link set up dev eth1
+
+echo "$STATUS_MSG: OK"
