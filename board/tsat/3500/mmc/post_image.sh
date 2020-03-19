b6fa27153447515a508020345fcf55f099887b41
Andreas Carlsen <alc@tsat.net>
2020-03-19 16:17:48 +0100

    add terminal and fpga to the mmc/qspi images

--- board/tsat/3500/mmc/post_image.sh
+++ board/tsat/3500/mmc/post_image.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
+# create the app partition filesystem
+output/host/sbin/mkfs.ext4 -v -d "$1/appfs" -E 'root_owner=0:0,lazy_itable_init=0,lazy_journal_init=0' -L 'APP' -O '^64bit' "$1/appfs.ext4" 256M
+
+# generate mmc full image
 support/scripts/genimage.sh -c board/tsat/3500/mmc/images/genimage.cfg
