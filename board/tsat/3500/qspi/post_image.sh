529f735a300a72d604deee0fb3d3519580498bc9
Andreas Carlsen <alc@tsat.net>
2020-03-19 16:17:48 +0100

    add terminal and fpga to the mmc/qspi images

--- board/tsat/3500/qspi/post_image.sh
+++ board/tsat/3500/qspi/post_image.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+# create the app partition filesystem
+output/host/sbin/mkfs.jffs2 -v -U -e 256 -l -d "$1/appfs" -o "$1/appfs.jffs2"
+
+# generate qspi full image
 cp -- board/tsat/3500/qspi/images/full.bif "$1"
 
 FULL_IMG='full_qspi.bin'
