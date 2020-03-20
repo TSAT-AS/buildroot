95d873c06b7b819d6f2bcccda1f4dc3e33b06c4d
Andreas Carlsen <alc@tsat.net>
2020-03-20 16:45:16 +0100

    add initial version of swupdate package generator

--- board/tsat/3500/mmc/post_image.sh
+++ board/tsat/3500/mmc/post_image.sh
@@ -2,9 +2,6 @@
 
 set -e
 
-# create boot-, env- and kernel filesystems
-support/scripts/genimage.sh -c board/tsat/3500/mmc/images/filesystems.cfg
-
 # create appfs filesystem
 APPFS_INPUT="$1/appfs"
 APPFS_OUTOUT="$1/appfs.ext4"
@@ -12,5 +9,15 @@ APPFS_SIZE='256M'
 test -d "$APPFS_INPUT" || exit 1
 output/host/sbin/mkfs.ext4 -v -d "$APPFS_INPUT" -E 'root_owner=0:0,lazy_itable_init=0,lazy_journal_init=0' -L 'APP' -O '^64bit' "$APPFS_OUTOUT" "$APPFS_SIZE"
 
+# create boot-, env- and kernel filesystems
+support/scripts/genimage.sh -c board/tsat/3500/mmc/images/filesystems.cfg
+
 # use generated filesystems and create mmc image
 support/scripts/genimage.sh -c board/tsat/3500/mmc/images/mmc.cfg
+
+# generate mmc swu packages
+cd -- "$1"
+export KEY="$HOST_DIR/usr/share/mkswu/private.pem"
+export POSTSCRIPT="$HOST_DIR/usr/share/mkswu/mmc-system-postinstall.sh"
+$HOST_DIR/bin/mkswu-mmc-system 'kernel.vfat' 'rootfs.ext4'
+$HOST_DIR/bin/mkswu-mmc-full 'mmc-system.swu' 'terminal/terminal.swu' 'fpga/fpga.swu'
--- board/tsat/3500/qspi/post_image.sh
+++ board/tsat/3500/qspi/post_image.sh
@@ -2,8 +2,11 @@
 
 set -e
 
-# create the app partition filesystem
-output/host/sbin/mkfs.jffs2 -v -U -e 256 -l -d "$1/appfs" -o "$1/appfs.jffs2"
+# create appfs filesystem
+APPFS_INPUT="$1/appfs"
+APPFS_OUTOUT="$1/appfs.jffs2"
+test -d "$APPFS_INPUT" || exit 1
+output/host/sbin/mkfs.jffs2 -v -U -e 256 -l -d "$APPFS_INPUT" -o "$APPFS_OUTOUT"
 
 # generate qspi full image
 cp -- board/tsat/3500/qspi/images/full.bif "$1"
@@ -13,3 +16,8 @@ echo "Creating full QSPI image: $1/$FULL_IMG"
 
 cd -- "$1"
 mkbootimage full.bif "$FULL_IMG"
+
+# generate qspi swu packages
+export KEY="$HOST_DIR/usr/share/mkswu/private.pem"
+$HOST_DIR/bin/mkswu-qspi-system 'uImage' 'devicetree.dtb' 'rootfs.squashfs'
+$HOST_DIR/bin/mkswu-qspi-full 'qspi-system.swu' 'terminal/terminal.swu' 'fpga/fpga.swu'
