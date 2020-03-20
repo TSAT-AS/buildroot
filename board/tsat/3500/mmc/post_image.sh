9d73b91eece43592540db4b8dbfbbb6003df9fbb
Andreas Carlsen <alc@tsat.net>
2020-03-20 12:30:56 +0100

    mmc post_image: split up filesystem generation and image building

--- board/tsat/3500/mmc/post_image.sh
+++ board/tsat/3500/mmc/post_image.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 
-# create the app partition filesystem
-output/host/sbin/mkfs.ext4 -v -d "$1/appfs" -E 'root_owner=0:0,lazy_itable_init=0,lazy_journal_init=0' -L 'APP' -O '^64bit' "$1/appfs.ext4" 256M
+set -e
 
-# generate mmc full image
-support/scripts/genimage.sh -c board/tsat/3500/mmc/images/genimage.cfg
+# create boot-, env- and kernel filesystems
+support/scripts/genimage.sh -c board/tsat/3500/mmc/images/filesystems.cfg
+
+# create appfs filesystem
+APPFS_INPUT="$1/appfs"
+APPFS_OUTOUT="$1/appfs.ext4"
+APPFS_SIZE='256M'
+test -d "$APPFS_INPUT" || exit 1
+output/host/sbin/mkfs.ext4 -v -d "$APPFS_INPUT" -E 'root_owner=0:0,lazy_itable_init=0,lazy_journal_init=0' -L 'APP' -O '^64bit' "$APPFS_OUTOUT" "$APPFS_SIZE"
+
+# use generated filesystems and create mmc image
+support/scripts/genimage.sh -c board/tsat/3500/mmc/images/mmc.cfg
