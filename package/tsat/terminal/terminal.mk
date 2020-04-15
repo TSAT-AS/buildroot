3fa03ae6cbf0775af7aa538fa0163e7cb713c1e0
Andreas Carlsen <alc@tsat.net>
2020-04-15 12:56:00 +0200

    terminal: update package to version that depends on an external bootgen utility

--- package/tsat/terminal/terminal.mk
+++ package/tsat/terminal/terminal.mk
@@ -7,9 +7,9 @@
 TERMINAL_SITE_METHOD = git
 TERMINAL_GIT_SUBMODULES = yes
 TERMINAL_SITE = ssh://git@dev.tsat.net:7999/tsat3k/terminal-converted.git
-TERMINAL_VERSION = 214740844a2185d8a69db37b3583d3d7bebb2f16
+TERMINAL_VERSION = 6a3b14f779c835dbfbc64b250a4f7223167fc8c4
 TERMINAL_CONF_OPTS = -DCMAKE_BUILD_TYPE=Release
-TERMINAL_DEPENDENCIES = host-cmake boost libnftnl libnl libopenssl libgpiod libiio
+TERMINAL_DEPENDENCIES = host-cmake host-bootgen boost libnftnl libnl libopenssl libgpiod libiio
 
 define TERMINAL_INSTALL_TARGET_CMDS
   $(HOST_DIR)/bin/cmake --build $(@D) --target package
