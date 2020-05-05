ebb8d3707640222ea3d582281a1edb04a5b074d6
Andreas Carlsen <alc@tsat.net>
2020-05-05 13:52:25 +0200

    rename u-boot to u-boot.elf to comply with bootgen utility

--- board/tsat/3500/common/post_image.sh
+++ board/tsat/3500/common/post_image.sh
@@ -2,6 +2,12 @@
 
 set -e
 
+# normalize U-Boot filename (for bootgen)
+UBOOT="$1/u-boot"
+if [ -f "$UBOOT" ]; then
+  mv -v -- "$UBOOT" "$UBOOT.elf"
+fi
+
 # normalize Linux kernel filename (for bootgen)
 KERNEL="$1/uImage"
 if [ -f "$KERNEL" ]; then
