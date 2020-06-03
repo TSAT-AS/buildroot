78e3516c2184795803e0d980e5705023fddeb27a
Andreas Carlsen <alc@tsat.net>
2020-06-03 12:00:53 +0200

    terminal-package: checkout experimental feature branch 'fpga_v250_1'

--- package/tsat/terminal/terminal.mk
+++ package/tsat/terminal/terminal.mk
@@ -7,7 +7,7 @@
 TERMINAL_SITE_METHOD = git
 TERMINAL_GIT_SUBMODULES = yes
 TERMINAL_SITE = ssh://git@dev.tsat.net:7999/tsat3k/terminal-converted.git
-TERMINAL_VERSION = 6a3b14f779c835dbfbc64b250a4f7223167fc8c4
+TERMINAL_VERSION = 0bd4b898ee78ed38fe6df587d6c8b324e49db991
 TERMINAL_CONF_OPTS = -DCMAKE_BUILD_TYPE=Release
 TERMINAL_DEPENDENCIES = host-cmake host-bootgen boost libnftnl libnl libopenssl libgpiod libiio
 
