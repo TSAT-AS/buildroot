3f342776d77ecc3f2507fc4c5016cda8130b523d
Andreas Carlsen <alc@tsat.net>
2020-06-09 13:12:47 +0200

    U-Boot: wrap boot script in FIT format

--- board/tsat/3500/qspi/post_image.sh
+++ board/tsat/3500/qspi/post_image.sh
@@ -9,6 +9,11 @@ test -d "$APPFS_INPUT" || exit 1
 test -f "$APPFS_OUTPUT" && rm "$APPFS_OUTPUT"
 output/host/sbin/mkfs.jffs2 -v -U -e 256 -l -d "$APPFS_INPUT" -o "$APPFS_OUTPUT"
 
+# build boot script FIT image
+cp -v -- board/tsat/3500/common/images/u-boot-script.its "$1"
+cp -v -- board/tsat/3500/qspi/uboot/boot_script.txt "$1"
+mkimage -f "$1/u-boot-script.its" "$1/u-boot-script.itb"
+
 # generate qspi full image
 cp -- board/tsat/3500/qspi/images/full.bif "$1"
 
