4da0624fbdecd576dd9cf55ec0460c0e2f8cd4d2
tsat-psv <43535969+tsat-psv@users.noreply.github.com>
2020-06-10 10:38:01 +0200

    tps65233: bump

--- package/tsat/tps65233/tps65233.mk
+++ package/tsat/tps65233/tps65233.mk
@@ -6,7 +6,7 @@
 
 TPS65233_SITE_METHOD = git
 TPS65233_SITE = ssh://git@dev.tsat.net:7999/lrsnmb/tps65233.git
-TPS65233_VERSION = cb617f7241c0cf88422e858c96d2ae4686747e47
+TPS65233_VERSION = 870fbab86fa41e9e8fa8b3ef777869f0ddff153d
 
 $(eval $(kernel-module))
 $(eval $(generic-package))
