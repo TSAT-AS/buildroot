619a494ac4427a1d466851a43040fecec13612ab
Bastien Auneau <bastien.auneau@while-true.fr>
2020-06-19 13:53:43 +0200

    Fix bug tps65233 return read variable from unsigned to signed

--- package/tsat/tps65233/tps65233.mk
+++ package/tsat/tps65233/tps65233.mk
@@ -6,7 +6,7 @@
 
 TPS65233_SITE_METHOD = git
 TPS65233_SITE = ssh://git@dev.tsat.net:7999/lrsnmb/tps65233.git
-TPS65233_VERSION = c69f9ff4c25ed22d76a653ccd87cce8739f5e595
+TPS65233_VERSION = 2c3e3963475ad6285656850c3a86804dee1c2590
 
 $(eval $(kernel-module))
 $(eval $(generic-package))
