2eddf36fd851c4bf7e6d044af29f4aa03129d405
Bastien Auneau <bastien.auneau@while-true.fr>
2020-07-03 16:51:55 +0200

    Update adrf6755: remove trace on open/close file because it is too verbose

--- package/tsat/adrf6755/adrf6755.mk
+++ package/tsat/adrf6755/adrf6755.mk
@@ -6,7 +6,7 @@
 
 ADRF6755_SITE_METHOD = git
 ADRF6755_SITE = ssh://git@dev.tsat.net:7999/lrsnmb/adrf6755.git
-ADRF6755_VERSION = 7ac3fbf41b9b4986394f40691068e9c47a3a9dae
+ADRF6755_VERSION = 472ea6a9dfb4295c985fc018957d5f7137de7eb6
 
 $(eval $(kernel-module))
 $(eval $(generic-package))
