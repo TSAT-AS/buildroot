e73a5a108a71aa58240454070db5807b996b3339
Bastien Auneau <bastien.auneau@while-true.fr>
2020-07-03 16:33:20 +0200

    Update adrf6755 commit hash

--- package/tsat/adrf6755/adrf6755.mk
+++ package/tsat/adrf6755/adrf6755.mk
@@ -6,7 +6,7 @@
 
 ADRF6755_SITE_METHOD = git
 ADRF6755_SITE = ssh://git@dev.tsat.net:7999/lrsnmb/adrf6755.git
-ADRF6755_VERSION = 1f197df2e7c533e888d00804ca3cbaf6880f1b71
+ADRF6755_VERSION = 7ac3fbf41b9b4986394f40691068e9c47a3a9dae
 
 $(eval $(kernel-module))
 $(eval $(generic-package))
