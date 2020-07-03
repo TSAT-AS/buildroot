664dcbb33f1a28bd07f6743332462b5d77f86049
Bastien Auneau <bastien.auneau@while-true.fr>
2020-07-03 15:40:07 +0200

    Update ADRF6755 commit hash (return 0xff as chip id if couldn't read i2c bus)

--- package/tsat/adrf6755/adrf6755.mk
+++ package/tsat/adrf6755/adrf6755.mk
@@ -6,7 +6,7 @@
 
 ADRF6755_SITE_METHOD = git
 ADRF6755_SITE = ssh://git@dev.tsat.net:7999/lrsnmb/adrf6755.git
-ADRF6755_VERSION = 1b4559ffa71e1b3113b79849aebf65079b3ff05d
+ADRF6755_VERSION = 7ac3fbf41b9b4986394f40691068e9c47a3a9dae
 
 $(eval $(kernel-module))
 $(eval $(generic-package))
