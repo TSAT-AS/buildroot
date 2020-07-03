d567f8ae95ee929363f31c9dff056f66e50522f7
Bastien Auneau <bastien.auneau@while-true.fr>
2020-07-03 16:55:37 +0200

    Update tps65233: no repeated START on i2c bus, much less verbose, protect file access with a mutex, fix sign/unsigned ioctl return code bug, add current limit source and level to ioctl

--- package/tsat/tps65233/tps65233.mk
+++ package/tsat/tps65233/tps65233.mk
@@ -6,7 +6,7 @@
 
 TPS65233_SITE_METHOD = git
 TPS65233_SITE = ssh://git@dev.tsat.net:7999/lrsnmb/tps65233.git
-TPS65233_VERSION = cb617f7241c0cf88422e858c96d2ae4686747e47
+TPS65233_VERSION = 0084471cdfb313ca5b249ef556e6b45d309eb21d
 
 $(eval $(kernel-module))
 $(eval $(generic-package))
