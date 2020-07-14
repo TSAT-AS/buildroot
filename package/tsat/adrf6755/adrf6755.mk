e8b9b8ea4996b0abb3e1704d18b820f5a2f358ce
Peter <psv@tsat.net>
2020-07-14 13:43:38 +0200

    revert to older ardf driver to avoid kernel panic

--- package/tsat/adrf6755/adrf6755.mk
+++ package/tsat/adrf6755/adrf6755.mk
@@ -6,7 +6,7 @@
 
 ADRF6755_SITE_METHOD = git
 ADRF6755_SITE = ssh://git@dev.tsat.net:7999/lrsnmb/adrf6755.git
-ADRF6755_VERSION = 1b4559ffa71e1b3113b79849aebf65079b3ff05d
+ADRF6755_VERSION = 4bd74502e0fa78800c4d8ecf1de8455d4968fec4
 
 $(eval $(kernel-module))
 $(eval $(generic-package))
