6d84dd6d8673753f33712724b963367fd94d88d1
Andreas Carlsen <alc@tsat.net>
2020-08-17 15:27:08 +0200

    Add SWU pub-key to target filesystem in post-build script
    - Adds support for choosing keys based on RELEASE mode

---
+++ board/tsat/3500/common/post_build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+SWU_KEY_SRC_DIR='board/tsat/3500/common/keys'
+SWU_KEY_DST_DIR="$TARGET_DIR/usr/local/share"
+SWU_KEY_DST_NAME='public.pem'
+
+if [ "$RELEASE" = "1" ]; then
+  SWU_KEY_SRC_NAME='swu-debug-pub.pem'
+else
+  SWU_KEY_SRC_NAME='swu-debug-pub.pem'
+fi
+
+cp -- "$SWU_KEY_SRC_DIR/$SWU_KEY_SRC_NAME" "$SWU_KEY_DST_DIR/$SWU_KEY_DST_NAME"
