f98dddbd40059b88fed7371f605bd9e385e910da
Andreas Carlsen <alc@tsat.net>
2020-09-24 11:35:57 +0200

    linux: add netfilter support

--- package/tsat/terminal/terminal.mk
+++ package/tsat/terminal/terminal.mk
@@ -9,7 +9,7 @@ TERMINAL_GIT_SUBMODULES = yes
 TERMINAL_SITE = ssh://git@dev.tsat.net:7999/tsat3k/terminal-converted.git
 TERMINAL_VERSION = 0b174eeb323a67fc28845b1a61f971fdb1bb37e1
 TERMINAL_CONF_OPTS = -DCMAKE_BUILD_TYPE=Release
-TERMINAL_DEPENDENCIES = host-cmake host-bootgen boost libnftnl libnl libopenssl libgpiod libiio
+TERMINAL_DEPENDENCIES = host-cmake host-bootgen boost nftables libnl libopenssl libgpiod libiio
 
 define TERMINAL_INSTALL_TARGET_CMDS
   $(HOST_DIR)/bin/cmake --build $(@D) --target package
