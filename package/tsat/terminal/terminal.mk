363ea103a5a0644ee038577b56881a400be6ab98
Andreas Carlsen <alc@tsat.net>
2020-10-27 21:55:59 +0100

    terminal package: bump

--- package/tsat/terminal/terminal.mk
+++ package/tsat/terminal/terminal.mk
@@ -7,7 +7,7 @@
 TERMINAL_SITE_METHOD = git
 TERMINAL_GIT_SUBMODULES = yes
 TERMINAL_SITE = ssh://git@dev.tsat.net:7999/tsat3k/terminal-converted.git
-TERMINAL_VERSION = 0b174eeb323a67fc28845b1a61f971fdb1bb37e1
+TERMINAL_VERSION = 1bdf189d62f6b6d997ef0edd8af6d0cea61d1201
 TERMINAL_CONF_OPTS = -DCMAKE_BUILD_TYPE=Release
 TERMINAL_DEPENDENCIES = host-cmake host-bootgen boost nftables libnl libopenssl libgpiod libiio
 
