bef1d13a942a3c553f3f66fbd8c3cfa0ed8fa7e4
Andreas Carlsen <alc@tsat.net>
2020-10-30 13:32:39 +0100

    bump mkswu package: make python 3.5 compatible

--- package/tsat/mkswu/mkswu.mk
+++ package/tsat/mkswu/mkswu.mk
@@ -6,7 +6,7 @@
 
 HOST_MKSWU_SITE_METHOD = git
 HOST_MKSWU_SITE = ssh://git@dev.tsat.net:7999/tsat3k/image-builder.git
-HOST_MKSWU_VERSION = 992315f69133cd2cbab5c65310e04bd1c51664ce
+HOST_MKSWU_VERSION = 201fcd7c93a9a540404874282aabf53d368beaa8
 HOST_MKSWU_DEPENDENCIES = host-openssl host-cpio
 
 define HOST_MKSWU_INSTALL_CMDS
