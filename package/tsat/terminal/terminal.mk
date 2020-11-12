a55d30313113e9ef894ebf1ab1efb007a2ecfb17
Andreas Carlsen <alc@tsat.net>
2020-11-12 21:01:05 +0100

    bump package: set terminal to version 35.2.0 and add package hash

--- package/tsat/terminal/terminal.mk
+++ package/tsat/terminal/terminal.mk
@@ -7,7 +7,7 @@
 TERMINAL_SITE_METHOD = git
 TERMINAL_GIT_SUBMODULES = yes
 TERMINAL_SITE = ssh://git@dev.tsat.net:7999/tsat3k/terminal-converted.git
-TERMINAL_VERSION = 1bdf189d62f6b6d997ef0edd8af6d0cea61d1201
+TERMINAL_VERSION = 35.2.0
 TERMINAL_CONF_OPTS = -DCMAKE_BUILD_TYPE=Release
 TERMINAL_DEPENDENCIES = host-cmake host-bootgen boost nftables libnl libopenssl libgpiod libiio
 
