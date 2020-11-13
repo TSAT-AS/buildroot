db35bd10ac39f862456a1eaf5fc4487595cc807e
Andreas Carlsen <alc@tsat.net>
2020-11-13 09:56:57 +0100

    sw-versions: add release- and system version
    * The 'sw-versions' file is associated with swupdate
      and contains the installed version of different components

--- board/tsat/3500/common/post_build.sh
+++ board/tsat/3500/common/post_build.sh
@@ -12,3 +12,13 @@ fi
 
 mkdir -p -- "$SWU_KEY_DST_DIR"
 cp -- "$SWU_KEY_SRC_DIR/$SWU_KEY_SRC_NAME" "$SWU_KEY_DST_DIR/$SWU_KEY_DST_NAME"
+
+VERSION_FILE="$TARGET_DIR/etc/sw-versions"
+BSP_GIT_DESCRIBE="$(cd $BASE_DIR/../.. && git describe --tags --long | sed 's/-[0-9]*-g/-/')"
+if [ -z $BSP_GIT_DESCRIBE ]; then
+  BSP_GIT_DESCRIBE='UNKNOWN'
+fi
+BUILDROOT_GIT_DESCRIBE="$(git describe --tags --long | sed 's/-[0-9]*-g/-/')"
+
+sed -i "s/@PLACEHOLDER_RELEASE_VERSION@/$BSP_GIT_DESCRIBE/g" $VERSION_FILE
+sed -i "s/@PLACEHOLDER_SYSTEM_VERSION@/$BUILDROOT_GIT_DESCRIBE/g" $VERSION_FILE
