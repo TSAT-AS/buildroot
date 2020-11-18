0e720199b8f1cc0eb868b25a59ff3a4315222325
Andreas Carlsen <alc@tsat.net>
2020-11-18 17:57:50 +0100

    load_keys.sh: add script that reads encryption key from OCM

---
+++ board/tsat/3500/common/rootfs_overlay/usr/local/bin/load_keys.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -e # fail fast
+
+KEY=""
+for i in $(seq 0 63); do
+    ADDR="$(printf "0x%x\n" $((0xffff0000 + i)))"
+    DATA="$(devmem "$ADDR" b | sed 's/0x/\\x/')"
+    KEY="${KEY:+${KEY}}${DATA}"
+done
+echo -en "$KEY" | /usr/bin/fscryptctl insert_key
+unset KEY
