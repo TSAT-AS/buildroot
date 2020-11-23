0bfcf08076243ac4a902169a2d5f8e15c5fea297
Andreas Carlsen <alc@tsat.net>
2020-11-23 16:47:01 +0100

    load_keys: clear OCM after reading key

--- board/tsat/3500/common/rootfs_overlay/usr/local/bin/load_keys.sh
+++ board/tsat/3500/common/rootfs_overlay/usr/local/bin/load_keys.sh
@@ -5,7 +5,8 @@ KEY=""
 for i in $(seq 0 63); do
     ADDR="$(printf "0x%x\n" $((0xffff0000 + i)))"
     DATA="$(devmem "$ADDR" b | sed 's/0x/\\x/')"
+    devmem "$ADDR" b 0
     KEY="${KEY:+${KEY}}${DATA}"
 done
 echo -en "$KEY" | /usr/bin/fscryptctl insert_key
-unset KEY
+unset KEY ADDR DATA
