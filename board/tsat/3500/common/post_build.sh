72d191e31145056162149db74afb91a413443885
Andreas Carlsen <alc@tsat.net>
2020-11-25 15:20:51 +0100

    swupdate: only expect signed SWUs in production

--- board/tsat/3500/common/post_build.sh
+++ board/tsat/3500/common/post_build.sh
@@ -1,17 +1,14 @@
 #!/bin/sh
 
-SWU_KEY_SRC_DIR='../keys'
-SWU_KEY_DST_DIR="$TARGET_DIR/usr/local/share"
-SWU_KEY_DST_NAME='public.pem'
-
+# add swupdate public key
 if [ "$BUILD_TYPE" = "PROD" ]; then
+  SWU_KEY_SRC_DIR='../keys'
   SWU_KEY_SRC_NAME='swu-release-pub.pem'
-else
-  SWU_KEY_SRC_NAME='swu-debug-pub.pem'
+  SWU_KEY_DST_DIR="$TARGET_DIR/usr/local/share"
+  SWU_KEY_DST_NAME='public.pem'
+  mkdir -p -- "$SWU_KEY_DST_DIR"
+  cp -- "$SWU_KEY_SRC_DIR/$SWU_KEY_SRC_NAME" "$SWU_KEY_DST_DIR/$SWU_KEY_DST_NAME"
 fi
-
-mkdir -p -- "$SWU_KEY_DST_DIR"
-cp -- "$SWU_KEY_SRC_DIR/$SWU_KEY_SRC_NAME" "$SWU_KEY_DST_DIR/$SWU_KEY_DST_NAME"
 
 VERSION_FILE="$TARGET_DIR/etc/sw-versions"
 BSP_GIT_DESCRIBE="$(cd $BASE_DIR/../.. && git describe --tags --long | sed 's/-[0-9]*-g/-/')"
