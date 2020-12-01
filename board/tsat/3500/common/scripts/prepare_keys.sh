362a817aa9e7d80d82366a43e5f6c68344bc040b
Andreas Carlsen <alc@tsat.net>
2020-12-01 13:51:51 +0100

    post-build: fix: decrypted key path

--- board/tsat/3500/common/scripts/prepare_keys.sh
+++ board/tsat/3500/common/scripts/prepare_keys.sh
@@ -4,7 +4,7 @@ set -e
 if [ "$BUILD_TYPE" = "PROD" ]; then
   cp -- ../keys/efuse.nky.enc "$1"
   cp -- ../keys/appfs.key.enc "$1"
-  gpg --decrypt --armor --output "$TMP_DIR/efuse.nky" "$1/efuse.nky.enc"
-  gpg --decrypt --armor --output "$TMP_DIR/appfs.key" "$1/appfs.key.enc"
+  gpg --decrypt --armor --output "/dev/shm/tmp/efuse.nky" "$1/efuse.nky.enc"
+  gpg --decrypt --armor --output "/dev/shm/tmp/appfs.key" "$1/appfs.key.enc"
   gpg-connect-agent 'scd killscd' /bye # force GPG to release Yubikey and let PIV be used
 fi
