ebe82f1113c3e074263c8f367ac8ac534558c9e1
Andreas Carlsen <alc@tsat.net>
2020-12-03 16:04:19 +0100

    gpg: make PIN configurable via env variable

--- board/tsat/3500/common/scripts/prepare_keys.sh
+++ board/tsat/3500/common/scripts/prepare_keys.sh
@@ -4,7 +4,15 @@ set -e
 if [ "$BUILD_TYPE" = "PROD" ]; then
   cp -- ../keys/efuse.nky.enc "$1"
   cp -- ../keys/appfs.key.enc "$1"
-  gpg --decrypt --armor --output "/dev/shm/tmp/efuse.nky" "$1/efuse.nky.enc"
-  gpg --decrypt --armor --output "/dev/shm/tmp/appfs.key" "$1/appfs.key.enc"
-  gpg-connect-agent 'scd killscd' /bye # force GPG to release Yubikey and let PIV be used
+
+  # unlock smart card
+  SERIAL="$(gpg-connect-agent 'scd serialno' /bye | grep -o 'SERIALNO [A-Z0-9]*' | cut -d' ' -f2)"
+  gpg-connect-agent 'OPTION pinentry-mode=loopback' "/let PIN $PIN" '/definq PASSPHRASE PIN' "scd checkpin $SERIAL" /bye
+
+  # decrypt keys
+  gpg --batch --yes --status-file "$1/keys.log" --decrypt --armor --output "/dev/shm/tmp/efuse.nky" "$1/efuse.nky.enc"
+  gpg --batch --yes --status-file "$1/keys.log" --decrypt --armor --output "/dev/shm/tmp/appfs.key" "$1/appfs.key.enc"
+
+  # force GPG/scdaemon to release Yubikey and let PIV be used
+  gpg-connect-agent 'scd killscd' /bye
 fi
