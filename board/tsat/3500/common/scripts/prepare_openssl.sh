1d01f5d392d17d5f46683ea65f42cac130d442df
Andreas Carlsen <alc@tsat.net>
2021-01-11 21:01:09 +0100

    post_image: raise error if PIN is not defined in production build

--- board/tsat/3500/common/scripts/prepare_openssl.sh
+++ board/tsat/3500/common/scripts/prepare_openssl.sh
@@ -2,6 +2,10 @@
 set -e
 
 if [ "$BUILD_TYPE" = "PROD" ]; then
+  if [ -z "$PIN" ]; then
+    echo "ERROR: PIN not defined"
+    exit 1
+  fi
   OPENSSL_CONF="$WORK/openssl.cnf"
   cp -- board/tsat/3500/production/openssl.cnf "$OPENSSL_CONF"
   sed -i "s#= BR_HOST#= $HOST_DIR#g" "$OPENSSL_CONF"
