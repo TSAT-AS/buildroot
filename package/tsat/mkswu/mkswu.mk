1fe357c20a2f59a0969c2d52bc8d2f1f45b6079c
Andreas Carlsen <alc@tsat.net>
2021-03-25 17:51:35 +0100

    bump mkswu: revert commit that is incompatible with build system

--- package/tsat/mkswu/mkswu.mk
+++ package/tsat/mkswu/mkswu.mk
@@ -6,7 +6,7 @@
 
 HOST_MKSWU_SITE_METHOD = git
 HOST_MKSWU_SITE = ssh://git@dev.tsat.net:7999/tsat3k/image-builder.git
-HOST_MKSWU_VERSION = 595d23bfe0df46bed20c7d57777ee7dd254dc3a4
+HOST_MKSWU_VERSION = ea2b76281f69819416552edbdddb2e1ff200ed31
 HOST_MKSWU_DEPENDENCIES = host-openssl host-cpio
 
 define HOST_MKSWU_INSTALL_CMDS
