43cb44049f3ef0a6300b4c38bbbdd00c013735f1
Andreas Carlsen <alc@tsat.net>
2021-03-25 17:31:43 +0100

    bump mkswu: bundle all version files with full.swu

--- package/tsat/mkswu/mkswu.mk
+++ package/tsat/mkswu/mkswu.mk
@@ -6,7 +6,7 @@
 
 HOST_MKSWU_SITE_METHOD = git
 HOST_MKSWU_SITE = ssh://git@dev.tsat.net:7999/tsat3k/image-builder.git
-HOST_MKSWU_VERSION = 6584d0d9232934347ace61b637fde4d33f8185c6
+HOST_MKSWU_VERSION = 595d23bfe0df46bed20c7d57777ee7dd254dc3a4
 HOST_MKSWU_DEPENDENCIES = host-openssl host-cpio
 
 define HOST_MKSWU_INSTALL_CMDS
